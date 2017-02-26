d146ca52fd948fceadf7790b8fc2ac504c502595
ModarTensai <ModarTensai@gmail.com>
2017-02-26 23:29:20 +0300

    Combine solver.py and l0solver.py

--- solver.py
+++ solver.py
@@ -1,14 +1,23 @@
-from l0_solver import PPA
+import numpy as np
+from scipy.linalg import eigh, svd
 from sklearn.linear_model import Lasso
 from abc import ABCMeta, abstractmethod
 
 class Solver(object):
+	'''Solves a linear combination problem (Ax=b) with some regularization if needed'''
 	__metaclass__ = ABCMeta
 	def __init__(self):
 		pass
 
 	@abstractmethod
 	def solve(self, A, b):
+		''' Solves Ax=b with regularization if needed.
+		Args:
+			A: An MxN matrix of feature vectors. 
+			b: An Mx1 matrix.
+		Returns:
+			The solution x.
+		'''
 		pass
 
 class LeastLinearSquaresSolver(Solver):
@@ -33,6 +42,60 @@ class ProximalL0Solver(Solver):
 	def __init__(self, l=1):
 		self.l = l
 
+	def largest_eigen_AtA(self, A, At):
+		#if one of the dimensions of A is small enough
+		if np.min(A.shape)<5000:
+			#consider an efficient method
+			if A.shape[0]<A.shape[1]:
+				AtA = np.matmul(A, At)
+				s = A.shape[0]
+			else:
+				AtA = np.matmul(At, A)
+				s = A.shape[1]
+			return eigh(AtA, eigvals_only=True, eigvals=(s-1, s-1))[0]
+		else: 
+			#consider memory saving method
+			_, s, _ = svd(A)
+			L = np.max(s)
+			return L*L
+
+	def PPA(self, A, b, x=None, l=1, iterations=100, history=False, verbose=False):
+		if history:
+			hist = []
+
+		#get the largest eigenvalue of AtA
+		At = np.transpose(A)
+		L = self.largest_eigen_AtA(A, At)
+
+		if x == None:
+			if len(b.shape)>1:
+				x = np.random.rand(A.shape[1], 1)
+			else:
+				x = np.random.rand(A.shape[1])
+
+		l2L = np.sqrt(2*l/L)
+		for i in range(iterations):
+			diff = np.matmul(A,x)-b
+			grad = np.matmul(At, diff)
+
+			if verbose or history:
+				fobj = 0.5*np.sum(diff**2)
+			if verbose:
+				print('iter:'+str(i)+', fobj:'+str(fobj))
+			if history:
+				hist.append(fobj + l*np.count_nonzero(x))
+
+			# x <- x - gradient/L
+			x = x - grad/L
+
+			# x <- argmin_{y} 0.5 ||y - x||_2^2 + l * ||y||_0
+			x[abs(x)<=l2L] = 0
+
+		if history:
+			return x, hist
+		else:
+			return x
+
 	def solve(self, A, b):
 		#optimize 0.5 || Ax-b||_2^2 + l * ||x||_0 using PPA and return the solution x
-		return PPA(A, b, l=self.l)
+		return self.PPA(A, b, l=self.l)
