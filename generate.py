ad46cf6dafdcc61f60332c6a5ee47fcfbf0d11d1
ModarTensai <ModarTensai@gmail.com>
2017-03-01 21:16:26 +0300

    Local generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import os
 import json
 import numpy as np
+from datetime import datetime
 from solver import LassoSolver
 from embedder import SkipThoughtEmbedder
 from decomposer import QuestionDecomposer
@@ -17,24 +18,7 @@ def load_questions(input_file):
 	with open(input_file, 'r') as f:
 		return json.load(f)
 
-def main(n):
-	dataset = load_questions(dataset_file)
-	questions = load_questions(questions_file)
-	chunk_size = int(np.ceil(float(len(questions))/num_threads))
-	questions = questions[n*chunk_size:(n+1)*chunk_size]
-	embedder = SkipThoughtEmbedder(dataset, load=embedded_dataset)
-	solver = LassoSolver(l=lmda)
-	decomposer = QuestionDecomposer(embedder, solver=solver)
-
-	print('decomposing '+str(len(questions))+' questions...')
-	basics = decomposer.decompose_all(questions)
-	data = [0]*len(questions)
-	for i in range(len(questions)):
-		data[i] = {
-			'question':questions[i],
-			'basic':[{'question':q,'score':s} for q, s in basics[i]]#[:top_N]
-		}
-
+def save(data, n):
 	print('saving to file...')
 	if not os.path.exists(output_folder):
 		os.makedirs(output_folder)
@@ -42,5 +26,38 @@ def main(n):
 		json.dump(data, f)
 	print('done')
 
-if __name__ == '__main__':
-	main(int(os.environ['SLURM_ARRAY_TASK_ID']))
+solver = None
+dataset = None
+embedder = None
+questions = None
+decomposer =None
+chunk_size = None
+
+def run(n):
+	global dataset, questions, chunk_size, embedder, solver, decomposer
+	ques = questions[n*chunk_size:(n+1)*chunk_size]
+
+	print('decomposing '+str(len(ques))+' questions...')
+	basics = decomposer.decompose_all(ques)
+	data = [0]*len(ques)
+	for i in range(len(ques)):
+		data[i] = {
+			'question':ques[i],
+			'basic':[{'question':q,'score':s} for q, s in basics[i]]#[:top_N]
+		}
+
+	return data
+
+dataset = load_questions(dataset_file)
+questions = load_questions(questions_file)
+chunk_size = int(np.ceil(float(len(questions))/num_threads))
+embedder = SkipThoughtEmbedder(dataset, load=embedded_dataset)
+solver = LassoSolver(l=lmda)
+decomposer = QuestionDecomposer(embedder, solver=solver)
+
+for i in range(500, 1000):
+	t = datetime.now()
+	print('Running job #'+str(i)+' ['+str(t)+']')
+	data = run(i)
+	save(data, i)
+	print('Total time '+str(datetime.now()-t))
