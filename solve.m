828be3b19bd95f16d51687f7ec1862ff5a0ff81a
ModarTensai <ModarTensai@gmail.com>
2017-03-02 02:38:22 +0300

    sequential run

--- solve.m
+++ solve.m
@@ -1,45 +1,58 @@
 close all; clear; clc;
-%% parameters
-lambda = 1e-5;
-num_jobs = feature('numCores');
-input_file = './models/AB.mat';
-output_file = './models/X.mat';
 
-%% loading
-disp('loding data...');
-load(input_file);
+chop = 0.5; %solve `chop`% of the data
 
-[m, n] = size(A);
-disp(['size of A is ', num2str(m), 'x', num2str(n)]);
-[m, q] = size(B);
-disp(['size of B is ', num2str(n), 'x', num2str(q)]);
+for o=1:ceil(10000/chop)
+    % parameters
+    lambda = 1e-5;
+    input_file = './AB.mat';
+    output_file = strcat(strcat('./X', num2str(o)),'.mat');
 
-%% preprocessing
-disp('preprocess B...');
-b = cell(size(B,2),1);
-for i=1:size(b,1)
-   b{i} = B(:,i); 
+	% loading
+    disp('loding data...');
+    load(input_file);
+
+    [~, q] = size(B);
+    chunk = ceil(q*chop/100);
+    if chunk*(o+1) <= q
+        B = B(:, chunk*o+1:chunk*(o+1));
+    else
+        B = B(:, chunk*o+1:q);
+    end
+
+    [m, n] = size(A);
+    disp(['size of A is ', num2str(m), 'x', num2str(n)]);
+    [m, q] = size(B);
+    disp(['size of B is ', num2str(m), 'x', num2str(q)]);
+
+	% preprocessing
+    disp('preprocess B...');
+    b = cell(size(B,2),1);
+    for i=1:size(b,1)
+       b{i} = B(:,i); 
+    end
+    clear B;
+
+    % solving
+    disp(['starting solver...']);
+    x = cell(q,1);
+    parfor i=1:q
+        disp(['[', datestr(now), ']', ' question #', num2str(i), '...']);
+        x{i} = lasso(A,b{i}, 'Lambda', lambda);
+    end
+    disp('done! ~AlhamduLeAllah');
+    clear A b;
+
+	% postprocessing
+    disp('postprocessing x...');
+    X = zeros(n, q);
+    for i=1:q
+       X(:,i) = x{i}; 
+    end
+    clear x;
+
+	% saving
+    disp(['saving x to file ', output_file]);
+    save(output_file, 'X');
+    clear X;
 end
-clear B;
-
-%% solving
-disp(['starting solver on ', num2str(num_jobs), ' CPUs...']);
-x = cell(q,1);
-parfor i=1:q
-    disp(['[', datestr(now), ']', ' question #', num2str(i), '...']);
-	x{i} = lasso(A,b{i}, 'Lambda', lambda);
-end
-disp('done! ~AlhamduLeAllah');
-clear A b;
-
-%% postprocessing
-disp('postprocessing x...');
-X = zeros(n, q);
-for i=1:q
-   X(:,i) = x{i}; 
-end
-clear x;
-
-%% saving
-disp(['saving x to file ', output_file]);
-save(output_file, 'X');
