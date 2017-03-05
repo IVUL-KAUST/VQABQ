9c2ab15cfab50ecc4360f1a67d9d6a2842eccdbd
JiaHong Huang <poppingstylek@gmail.com>
2017-03-06 01:26:11 +0300

    Shaheen 800 nodes run

--- generate.py
+++ generate.py
@@ -9,29 +9,30 @@ from decomposer import QuestionDecomposer
 
 top_N = 20
 lmda = 1e-5
+num_chunks = 100
 dataset_file = './data/vqa_train_val_questions.json'
 questions_file ='./data/vqa_test_questions.json'
 output_folder = './data/basic_vqa_questions/'
 embedded_dataset = './models/skipthoughts_vqa_train_val_dataset.npy'
 
-num_tasks = 16
-num_threads = 100
-thread_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
-task_id = int(sys.argv[1])
+job_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
+core_id = int(sys.argv[1])
+node_id = job_id%8
+chunk_id = job_id//8
 
 def load_questions(input_file):
 	with open(input_file, 'r') as f:
 		return json.load(f)
 
 questions = load_questions(questions_file)
-chunk_size = int(np.ceil(float(len(questions))/num_threads))
-questions = questions[(thread_id*chunk_size):((thread_id+1)*chunk_size)]
+chunk_size = int(np.ceil(float(len(questions))/num_chunks))
+questions = questions[(chunk_id*chunk_size):((chunk_id+1)*chunk_size)]
 if len(questions) == 0:
 	exit()
 
 if not os.path.exists(output_folder):
 	os.makedirs(output_folder)
-path = output_folder+str(thread_id)+'/'
+path = output_folder+str(chunk_id)+'/'
 if not os.path.exists(path):
 	os.makedirs(path)
 
@@ -40,16 +41,15 @@ embedder = SkipThoughtEmbedder(dataset, load=embedded_dataset)
 solver = LassoSolver(l=lmda)
 decomposer = QuestionDecomposer(embedder, solver=solver)
 
-print('decomposing '+str(len(questions))+' questions...')
-for i in range(len(questions)):
-	if i%num_tasks != task_id:
-		continue
-
+questions_list = reversed(range(len(questions)))
+questions_list = [q for q in questions_list if (q//8)%8==node_id and q%8==core_id]
+print('decomposing '+str(len(questions_list))+' questions...')
+for i in questions_list:
 	file = path+str(i)+'.json'
 	if os.path.isfile(file):
 		continue
 
-	print('question #'+str(thread_id*chunk_size+i)+' ['+str(datetime.now())+']')	
+	print('question #'+str(chunk_id*chunk_size+i)+' ['+str(datetime.now())+']')	
 	basic = decomposer.decompose(questions[i])
 	data = {
 		'question':questions[i],
--- run.sh
+++ run.sh
@@ -7,12 +7,14 @@
 #                                  #
 ####################################
 
-#SBATCH --array=0-99
-#SBATCH --workdir=/home/alfadlmm/VQA/
+#SBATCH --account=k1216
+#SBATCH --array=0-799
+#SBATCH --partition=workq
+#SBATCH --workdir=/scratch/alfadlmm/
 #SBATCH --job-name=VQA    # Job name
 #SBATCH --output=./log/%j.out # Stdout (%j expands to jobId)
 #SBATCH --error=./log/%j.err # Stderr (%j expands to jobId)
-#SBATCH --mem=220G   # memory per NODE
+#SBATCH --mem=120G   # memory per NODE
 #SBATCH --time=2-00:00:00   # walltime
 
 ## LOAD MODULES ##
@@ -20,10 +22,14 @@ module purge		# clean up loaded modules
 
 # load necessary modules
 module load slurm
-module load anaconda
+
+# load miniconda and activate python virtual environment vqa
+# created by conda --create
+export PATH="/scratch/alfadlmm/miniconda2/bin:$PATH"
+source activate vqa
 
 ## RUN YOUR PROGRAM ##
-for i in {0..15}
+for i in {0..7}
 do
 	srun python generate.py $i &
 done
