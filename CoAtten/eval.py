876c22263a2b91d8dd4f59b0419b1812fe3f029f
ModarTensai <ModarTensai@gmail.com>
2017-03-07 02:45:16 +0300

    VQAEvaluator class

---
+++ CoAtten/eval.py
@@ -0,0 +1,61 @@
+from vqa import CoAttenVQA
+
+class VQAEvaluator(object):
+	'''Evaluates a VQA dataset of questions and generate the answers'''
+	def __init__(self, vqa=None, concatenate=None):
+		'''Initializes the VQAEvaluator
+
+		Args:
+			vqa: A VQA object.
+			concatenate: A function that take a question and a list of basic questions
+					with a score value and append them into one question
+					basic = [{'question':'DUMMY'
+							  'score':0.123
+					}]
+		'''
+		if vqa == None:
+			self.__vqa = CoAttenVQA()
+		else:
+			self.__vqa = vqa
+		if concatenate == None:
+			self.__concat = VQAEvaluator.__concatenate
+		else:
+			self.__concat = concatenate
+
+	@staticmethod
+	def __concatenate(question, basic):
+		'''Concatenate the main question with the basic questions
+
+		Args:
+			question: The main question.
+			basic: The list of basic questions with their similarity score.
+		Returns:
+			The concatenated question.
+		'''
+		basic = [b['question'] for b in basic]
+		return question1+' '+' '.join(basic)
+
+	def evaluate(self, dataset):
+		'''Compute the answers of the dataset and return them in a list
+		
+		Args:
+			dataset: The dataset should have the following structure
+				dataset = [{'question_id':123,
+							'question':'DUMMY', 
+							'image_path':'DUMMY', 
+							'basic':[
+								{'question':'DUMMY','score':0.123}
+							]
+				}]
+		Returns:
+			The list of answers as
+				result = [{'question_id':123, 'answer':'DUMMY'}]
+		'''
+		result = [0]*len(dataset)
+		for i in range(len(dataset)):
+			d = dataset[i]
+			image_path = d['image_path']
+			quesiton = self.__concat(d['question'], d['basic'])
+			answer = self.__vqa.answer(image_path, quesiton)
+			result[i] = {'question_id':d['question_id'], 'answer':answer}
+		return result
