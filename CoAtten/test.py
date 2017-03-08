721bc4326bb83d2389cdefd5f7585311ef8b5eb4
ModarTensai <ModarTensai@gmail.com>
2017-03-09 02:04:14 +0300

    Union concatenation

--- CoAtten/test.py
+++ CoAtten/test.py
@@ -8,17 +8,42 @@ devtest = '/home/modar/VQA/data/OpenEnded_mscoco_test-dev2015_basic_questions.js
 output_file = '/home/modar/VQA/data/dev_test2015_answers.json'
 
 
-def concatenate(question, basic):
-		basic = [b for b in basic if b['score']>0]
-		basic = [b['question'] for b in basic]
-		if basic[0]==question:
-			basic = basic[1:]
-		strn = question+' '+' '.join(basic)
-		strn = strn.split(' ')
-		strn = strn[:26]
-		return ' '.join(strn)
+def concat0(question, basic):
+	#No concatenation
+	return question
 
-vqa = VQAEvaluator(concatenate=concatenate)
+def concat1(question, basic):
+	#Appending the questions that is not
+	#exactly the main question and with score > 0
+	#then take the first 26 words in the concatenation
+	basic = [b for b in basic if b['score']>0]
+	basic = [b['question'] for b in basic]
+	if basic[0]==question:
+		basic = basic[1:]
+	strn = question+' '+' '.join(basic)
+	strn = strn.split(' ')
+	strn = strn[:26]
+	return ' '.join(strn)
+
+def concat2(question, basic):
+	#Take the questions that is not
+	#exactly the main question and with score > 0
+	#then take the first 26 unique words in the concatenation
+	basic = [b for b in basic if b['score']>0]
+	basic = [b['question'] for b in basic]
+	if basic[0]==question:
+		basic = basic[1:]
+	strn = question+' '+' '.join(basic)
+	strn = set(strn.split(' '))
+	cont = strn.pop()
+	for i in range(min(25, len(strn))):
+		cont = cont+' '+strn.pop()
+	return cont
+
+#pick your concatenation method
+method = concat2
+
+vqa = VQAEvaluator(concatenate=method)
 
 with open(devtest, 'r') as f:
 	dataset = json.load(f)
@@ -31,10 +56,10 @@ with open(output_file, 'w') as f:
 def show(i, view=True):
 	question = dataset[i]['question']
 	basic = dataset[i]['basic']
-	concatenated = concatenate(question, basic)
+	concatenated = method(question, basic)
 	answer = data[i]['answer']
 	print('Question: '+question)
-	print('Concatenated: '+concatenated)
+	print('Concatenated: '+method)
 	print('Answer: '+answer)
 	if view:
 		image_file = images_folder+dataset[i]['image_path']
